f89dd5a673de4e642b3e795298692772d62b80de
deeppandey <55391487+deeppandey@users.noreply.github.com>
2023-10-17 20:03:16 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Party Cruise Ship</title>
+	<!-- google font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com/">
+	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap"
+		rel="stylesheet">
+	<!-- favicons Icons -->
+	<link rel="apple-touch-icon" sizes="180x180" href="web_assets/images/logo.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="web_assets/images/logo.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="web_assets/images/logo.png" />
+	<link rel="manifest" href="web_assets/images/favicons/site.webmanifest" />
+	<!-- plugins css -->
+	<link rel="stylesheet" href="web_assets/vendors/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="web_assets/vendors/fontawesome/css/all.min.css">
+	<link rel="stylesheet" href="web_assets/vendors/jarallax/jarallax.css">
+	<link rel="stylesheet" href="web_assets/vendors/animate/animate.min.css">
+	<link rel="stylesheet" href="web_assets/vendors/owl-carousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="web_assets/vendors/owl-carousel/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="web_assets/vendors/youtube-popup/youtube-popup.css">
+	<link rel="stylesheet" href="web_assets/vendors/cretech-icons/style.css">
+	<link rel="stylesheet" href="web_assets/vendors/jquery-nice-select/css/nice-select.css">
+	<!-- templates css -->
+	<link rel="stylesheet" href="web_assets/css/cretech.css">
+</head>
+
+<body class="custom-cursor">
+
+	<div class="custom-cursor__cursor"></div>
+	<div class="custom-cursor__cursor-two"></div>
+
+	
+	<div class="page-wrapper">
+		<div class="header-three">
+			
+			<nav class="main-menu sticky-header">
+				<div class="container-fluid">
+					<div class="main-menu__logo">
+						<!-- <a href="index.php">
+							<img src="web_assets/images/" style="width:190px;" alt="Cretech">
+						</a> -->
+						<h3 style="color: red"> Party Cruise Ship </h3>
+					</div><!-- /.main-menu__logo -->
+
+					<ul class="main-menu__list">
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+						<li >
+							<a href="#about">About</a>
+						</li>
+						<li>
+							<a href="#service">Services</a>
+						</li>
+						<li >
+							<a href="#why">Book Form</a>
+						</li>
+						
+						
+					</ul><!-- /.main-menu__list -->
+
+					<div class="main-menu__right">
+						<a href="#" class="mobile-nav__toggler">
+							<span></span>
+							<span></span>
+							<span></span>
+						</a>
+						
+						<a href="#" class="main-menu__cta">
+							<i class="fa fa-phone-alt"></i>
+							<span class="main-menu__cta__text">
+								<b>+ 91 12345689078</b>
+								Call for booking
+							</span><!-- /.main-menu__cta__text -->
+						</a><!-- /.main-menu__cta -->
+					</div><!-- /.main-menu__right -->
+
+				</div><!-- /.container-fluid -->
+			</nav><!-- /.main-menu -->
+		</div><!-- /.header-three -->
