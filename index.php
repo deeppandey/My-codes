0f8662ba3f6972325b9aaef33e77a5ee67bf3f0e
deeppandey <55391487+deeppandey@users.noreply.github.com>
2023-10-17 20:03:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<?php include('header.php');?>
+
+		<div class="slider-one slider-three">
+			<div class="slider-one__carousel owl-carousel owl-theme thm-owl__carousel"
+				data-owl-options='{"loop": true, "items": 1, "navText": ["<span class=\"fa fa-angle-left\"></span>","<span class=\"fa fa-angle-right\"></span>"], "margin": 0, "dots": true, "nav": true, "animateOut": "slideOutDown", "animateIn": "fadeIn", "active": true, "smartSpeed": 1000, "autoplay": true, "autoplayTimeout": 7000, "autoplayHoverPause": false}'>
+				<div class="item slider-one__slide-1">
+					<div class="slider-one__bg" style="background-image: url(web_assets/images/background/slider-3-1.jpg);">
+					</div><!-- /.slider-one__bg -->
+					<div class="slider-one__shape-1"></div><!-- /.slider-one__shape-1 -->
+					<div class="slider-one__shape-2"></div><!-- /.slider-one__shape-2 -->
+					<div class="container">
+						<div class="slider-one__content text-center">
+							<div class="slider-one__floated lettering-text">Party Cruise Ship</div>
+							<!-- /.slider-one__floated -->
+							
+							<div class="slider-one__title-wrapper">
+								<h2 class="slider-one__title">Welcome to <br>Party Cruise Ship</h2>
+								<!-- /.slider-one__title -->
+							</div><!-- /.slider-one__title-wrapper -->
+							<p class="slider-one__text">Book our ship to better place and party. <br>Party Cruise Ship is a wonderful and honorable way to generate revenue.</p><!-- /.slider-one__text -->
+							<div class="slider-one__btns">
+								<a href="#" class="thm-btn slider-one__btn"><span>Register</span></a>
+								<!-- /.thm-btn slider-one__btn -->
+							</div><!-- /.slider-one__btns -->
+						</div><!-- /.slider-one__content -->
+					</div><!-- /.container -->
+				</div><!-- /.item -->
+				<div class="item slider-one__slide-2">
+					<div class="slider-one__bg" style="background-image: url(web_assets/images/background/slider-3-2.jpg);">
+					</div><!-- /.slider-one__bg -->
+					<div class="slider-one__shape-1"></div><!-- /.slider-one__shape-1 -->
+					<div class="slider-one__shape-2"></div><!-- /.slider-one__shape-2 -->
+					<div class="container">
+						<div class="slider-one__content text-center">
+							<div class="slider-one__floated lettering-text">Party Cruise Ship</div>
+							<!-- /.slider-one__floated -->
+							
+							<div class="slider-one__title-wrapper">
+								<h2 class="slider-one__title">Welcome  to<br> Party Cruise Ship</h2>
+								<!-- /.slider-one__title -->
+							</div><!-- /.slider-one__title-wrapper -->
+							<p class="slider-one__text">Meet the outstanding performers in our <br> industry-award-winning team of professionals.</p><!-- /.slider-one__text -->
+							<div class="slider-one__btns">
+								<a href="#" class="thm-btn slider-one__btn"><span>Register</span></a>
+								<!-- /.thm-btn slider-one__btn -->
+							</div><!-- /.slider-one__btns -->
+						</div><!-- /.slider-one__content -->
+					</div><!-- /.container -->
+				</div><!-- /.item -->
+			</div><!-- /.slider-one__carousel -->
+		</div><!-- /.slider-one -->
+
+		<section class="section-padding--bottom section-padding--top" id="service">
+			<div class="container">
+                  <div class="section-title text-center">
+					<p class="section-title__text">Our Services</p><!-- /.section-title__text -->
+					<h2 class="section-title__title">We have cruise ship is available for charter and caters to following Audiences..</h2><!-- /.section-title__title -->
+				</div>
+				<div class="row gutter-y-30">
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Birthday Parties</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Bridal Showers</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Baby Showers</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Kitty Parties</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Seminars</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							<br>
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Bachelorette Parties</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Wedding Anniversaries</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="service-card-two gray-bg">
+							<h3 class="service-card-two__title"><a href="#">Corporate Events</a></h3>
+							<!-- /.service-card-two__title -->
+							
+							<br>
+						</div><!-- /.service-card-two -->
+					</div><!-- /.col-lg-3 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section>
+<br><br>
+		<section class="section-padding--bottom about-six" id="about">
+			<div class="container">
+				<div class="row gutter-y-60">
+					<div class="col-lg-6">
+						<div class="about-six__content">
+							<div class="section-title">
+								<p class="section-title__text">About US Party Cruise Ship</p><!-- /.section-title__text -->
+								<!-- <h2 class="section-title__title">Best Talent, Competitive
+									Cast, Incredible IT Service
+									Infrastructure</h2> -->
+							</div><!-- /.section-title -->
+							<div class="about-six__text">Party Cruise Ship are large passenger ships used mainly for vacationing. Unlike ocean liners, which are used for transport, cruise ships typically embark on round-trip voyages to various ports of call, where passengers may go on tours known as "shore excursions". On "cruises to nowhere" or "nowhere voyages", some cruise ships make two- to three-night round trips without visiting any ports of call.<br>
+                  [1] Cruiseferry MS Galaxy at the port of Mariehamn, Åland, in February 2016
+Modern cruise ships tend to have less hull strength, speed, and agility compared to ocean liners.<br>[2] However, they have added amenities to cater to water tourists, with recent vessels being described as "balcony-laden floating condominiums".</div><!-- /.about-six__text -->
+							
+							
+						</div><!-- /.about-six__content -->
+					</div><!-- /.col-lg-6 -->
+					<div class="col-lg-6">
+						<div class="about-six__images wow fadeInUp" data-wow-duration="1500ms">
+							<img src="web_assets/images/resources/about-six-1-1.jpg" alt="">
+							<img src="web_assets/images/resources/about-six-1-2.jpg" alt="">
+						</div><!-- /.about-six__images -->
+					</div><!-- /.col-lg-6 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section>
+
+		
+
+		
+		<section
+			class="section-padding--bottom section-padding--top gray-bg testimonials-one background-repeat-no background-position-top-center"
+			style="background-image: url(web_assets/images/shapes/testi-bg-2-1.png);">
+			<div class="container">
+				<div class="section-title text-center">
+					<p class="section-title__text">Our clients</p><!-- /.section-title__text -->
+					<h2 class="section-title__title">Trusted Worldwide Peoples</h2><!-- /.section-title__title -->
+				</div><!-- /.section-title -->
+				<div class="row gutter-y-30">
+					<div class="col-lg-6">
+						<div class="testimonials-one-card">
+							<div class="testimonials-one-card__image">
+								<img src="web_assets/images/resources/testi-1-1.jpg" alt="">
+							</div><!-- /.testimonials-one-card__image -->
+							<div class="testimonials-one-card__content">
+								<div class="testimonials-one-card__text">Anyone who loves or pursues desires to obtain pain of because it, but because occasionally circumstances can procure him some great pleasure.</div>
+								<!-- /.testimonials-one-card__text -->
+								<h3 class="testimonials-one-card__title">Michal Rahul</h3>
+								<!-- /.testimonials-one-card__title -->
+								<p class="testimonials-one-card__designation">Ul - UX Designer</p>
+								<!-- /.testimonials-one-card__designation -->
+								<i class="icon-right-quote testimonials-one-card__icon"></i>
+							</div><!-- /.testimonials-one-card__content -->
+						</div><!-- /.testimonials-one-card -->
+					</div><!-- /.col-lg-6 -->
+					<div class="col-lg-6">
+						<div class="testimonials-one-card">
+							<div class="testimonials-one-card__image">
+								<img src="web_assets/images/resources/testi-1-2.jpg" alt="">
+							</div><!-- /.testimonials-one-card__image -->
+							<div class="testimonials-one-card__content">
+								<div class="testimonials-one-card__text">On the other hand denounc with ghteo
+									indignation and dislike men who so beguiled and Party Cruise Shipralized the charms of pleasure
+									the momen blinded by desire cannot foresee the pain and trouble.</div>
+								<!-- /.testimonials-one-card__text -->
+								<h3 class="testimonials-one-card__title">Jessica Brown</h3>
+								<!-- /.testimonials-one-card__title -->
+								<p class="testimonials-one-card__designation">Ul - UX Designer</p>
+								<!-- /.testimonials-one-card__designation -->
+								<i class="icon-right-quote testimonials-one-card__icon"></i>
+							</div><!-- /.testimonials-one-card__content -->
+						</div><!-- /.testimonials-one-card -->
+					</div><!-- /.col-lg-6 -->
+				</div><!-- /.ro -->
+			</div><!-- /.container -->
+		</section>
+
+
+		
+
+	
+
+		
+
+		</div><!-- /.client-carousel -->
+
+	<?php include('footer.php');?>
