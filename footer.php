972320c33e03093186b9e08c46b2b1d5153060b3
deeppandey <55391487+deeppandey@users.noreply.github.com>
2023-10-17 20:03:01 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,133 @@
+	<div class="footer-main section-padding--bottom section-padding--top">
+			<div class="container">
+				<div class="row gutter-y-30">
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--about">
+							<h3 style="color: white;"> Party Cruise Ship </h3> 
+							<ul class="footer-widget__social">
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+								<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+							</ul><!-- /.footer-widget__social -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--links">
+							<h3 class="footer-widget__title">Important Links</h3>
+							
+							<ul class="footer-widget__links">
+								<li>
+									<a href="index.php">Home</a>
+								</li>
+								<li>
+									<a href="#about">About</a>
+								</li>
+								<li>
+									<a href="#service">Services</a>
+								</li>
+								
+							</ul><!-- /.footer-widget__links -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--newsletter">
+							<h3 class="footer-widget__title">Contact Links</h3>
+							<ul class="footer-widget__links">
+							
+								<li>
+									<a href="#">Login</a>
+								</li>
+								<li>
+									<a href="#">Register</a>
+								</li>
+							</ul><!-- /.footer-widget__links -->
+							
+							<div class="mc-form__response"></div><!-- /.mc-form__response -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--contact">
+							<h3 class="footer-widget__title">Contact</h3><!-- /.footer-widget__title -->
+							<ul class="footer-widget__contact">
+								<li>
+									<i class="fa fa-phone"></i>
+									<a href="#">+91  XXXXXXXXX</a>
+								</li>
+								<li>
+									<i class="fa fa-envelope"></i>
+									<a href="#">info@PartyCruiseShip.in</a>
+								</li>
+								<li>
+									<i class="fa fa-person"></i>
+									Owner Name: Deepak Pandey <br> 
+								</li>
+							</ul><!-- /.footer-widget__contact -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</div><!-- /.footer-main -->
+		<div class="footer-bottom">
+			<div class="container">
+				<p>&copy; All Copyright <span class="dynamic-year"></span> by Party Cruise Ship</p>
+			</div><!-- /.container -->
+		</div><!-- /.footer-bottom -->
+	</div><!-- /.page-wrapper -->
+
+
+	<div class="mobile-nav__wrapper">
+		<div class="mobile-nav__overlay mobile-nav__toggler"></div>
+		<!-- /.mobile-nav__overlay -->
+		<div class="mobile-nav__content">
+			<a href="#" class="mobile-nav__close mobile-nav__toggler">
+				<span></span>
+				<span></span>
+			</a>
+
+			<div class="logo-box">
+				<a href="index.php" aria-label="logo image"><img src="web_assets/images/logo.png" width="98"
+						height="100" alt="Cretech"></a>
+			</div>
+			<!-- /.logo-box -->
+			<div class="mobile-nav__container"></div>
+			<!-- /.mobile-nav__container -->
+
+			
+			<ul class="mobile-nav__social">
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+			</ul><!-- /.mobile-nav__social -->
+
+
+
+		</div>
+		<!-- /.mobile-nav__content -->
+	</div>
+	<!-- /.mobile-nav__wrapper -->
+
+
+	<a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+
+	<!-- plugins js -->
+	<script src="web_assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+	<script src="web_assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="web_assets/vendors/jquery-validation/jquery.validate.min.js"></script>
+	<script src="web_assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="web_assets/vendors/wow/wow.js"></script>
+	<script src="web_assets/vendors/jarallax/jarallax.min.js"></script>
+	<script src="web_assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
+	<script src="web_assets/vendors/lettering-js/jquery.lettering.js"></script>
+	<script src="web_assets/vendors/jquery-appear/jquery.appear.min.js"></script>
+	<script src="web_assets/vendors/youtube-popup/youtube-popup.jquery.js"></script>
+	<script src="web_assets/vendors/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+	<!-- template js -->
+	<script src="web_assets/js/cretech.js"></script>
+
+</body>
+
+
+
+</html>
